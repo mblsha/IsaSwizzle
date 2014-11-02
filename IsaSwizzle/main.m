e3631b808d02d029920c6123611619bfd1e7c0aa
Michail Pishchagin <mblsha@yandex-team.ru>
2014-11-02 10:33:19 +0300

    Initial Commit

---
+++ IsaSwizzle/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IsaSwizzle
+//
+//  Created by Michail Pishchagin on 02.11.14.
+//  Copyright (c) 2014 Michail Pishchagin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
